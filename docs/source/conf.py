7e068bf098e61feefd523505df75ae2ea48b4a86
derekpierre <derek.pierre@gmail.com>
2022-02-01 14:56:07 -0500

    Add Threshold logo and purple colour, and remove version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,40 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = '.rst'
+
+# The main toctree document.
+main_doc = 'index'
+
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+
+}
+
+html_logo = '.static/img/threshold_icon_purple.png'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['.static']
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+
+def setup(app):
+    app.add_css_file('style.css')
